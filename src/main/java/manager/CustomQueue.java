a375e285577a3fe271620e9e3564b28dfcdc709e
IT24101297 <it24101297@my.sliit.lk>
2025-05-19 00:22:38 +0530

    Create CustomQueue.java

---
+++ src/main/java/manager/CustomQueue.java
@@ -0,0 +1,86 @@
+package util;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CustomQueue<T> implements Iterable<T> {
+    private T[] queue;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+
+    @SuppressWarnings("unchecked")
+    public CustomQueue() {
+        this.capacity = 10; // Initial capacity
+        this.queue = (T[]) new Object[capacity];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public void enqueue(T item) {
+        if (size == capacity) {
+            resize();
+        }
+        rear = (rear + 1) % capacity;
+        queue[rear] = item;
+        size++;
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        T item = queue[front];
+        queue[front] = null; // Help garbage collection
+        front = (front + 1) % capacity;
+        size--;
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        int newCapacity = capacity * 2;
+        T[] newQueue = (T[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newQueue[i] = queue[(front + i) % capacity];
+        }
+        queue = newQueue;
+        front = 0;
+        rear = size - 1;
+        capacity = newCapacity;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new QueueIterator();
+    }
+
+    private class QueueIterator implements Iterator<T> {
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < size;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T item = queue[(front + currentIndex) % capacity];
+            currentIndex++;
+            return item;
+        }
+    }
+}
